4d1e82ed65bb8e184644343f8a5d5373cb3d52f1
T0B1M4N <tobimanczv@seznam.cz>
2025-05-21 14:55:44 +0200

    basic ui

---
+++ src/SideBar.jsx
@@ -0,0 +1,22 @@
+import { useState, useTransition } from 'react'
+import './SideBar.css'
+
+function SideBar() {
+  return(
+    <>
+       <div className="main">
+          <div className="Sidebutton">1</div>
+          <div className="Sidebutton">2</div>
+          <div className="Sidebutton">3</div>
+          <div className="Sidebutton">4</div>
+          <div className="Sidebutton">5</div>
+          <div className="Sidebutton">6</div>
+          <div className="Sidebutton">7</div>
+          <div className="Sidebutton">8</div>
+          <div className="Sidebutton">9</div>
+       </div>
+    </>
+  )
+}
+
+export default SideBar
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import SideBar from './SideBar.jsx'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('TopBar')).render(
   <StrictMode>
     <App />
+  </StrictMode>,
+)
+
+createRoot(document.getElementById('SideBarDiv')).render(
+  <StrictMode>
+    <SideBar />
   </StrictMode>,
 )
