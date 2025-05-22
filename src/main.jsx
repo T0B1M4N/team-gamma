9a699278c34febfb45f9e895221d2a03924829ab
Ondra <ondra7m@gmail.com>
2025-05-22 13:30:18 +0200

    Přidání Cart Contextu a lokalizace

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { CartProvider } from './context/CartContext'; 
-import './i18n';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <CartProvider>
-      <App />
-    </CartProvider>
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
