fa5da733dd3d76b8d4d3c4069f7ed5e1888a131c
Ondra <ondra7m@gmail.com>
2025-05-23 13:39:21 +0200

    fix  ondra

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,13 @@ import TopBar from './TopBar.jsx'
 import SideBar from './SideBar.jsx'
 import Products from './products.jsx'
 import BottomBar from "./BottomBar.jsx"
+import './i18n'
+
+import { CartProvider } from './context/CartContext.jsx'
+import { I18nextProvider } from 'react-i18next'
+import i18n from "./i18n"
+
+
 
 createRoot(document.getElementById('TopBar')).render(
   <StrictMode>
